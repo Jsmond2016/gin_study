1f20469b1c5ce1f0e4437568e09e60f7d4a13520
Jsmond2016 <Jsmond2016@gmail.com>
2022-03-26 11:55:30 +0800

    feat: chapter-02-form 表单绑定

--- chapter-02/main-model.go
+++ chapter-02/main-model.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Login struct {
-	Name     string `form:"name" json:"name" uri:"user" xml:"user"  binding:"required"`
+	Name     string `form:"name" json:"name" uri:"name" xml:"name"  binding:"required"`
 	Password string `form:"password" json:"password" uri:"password" xml:"password" binding:"required"`
 }
 
@@ -15,7 +15,7 @@ func main() {
 	router := gin.Default()
 	//1.binding JSON
 	// Example for binding JSON ({"user": "hanru", "password": "hanru123"})
-	router.POST("/login", func(c *gin.Context) {
+	router.POST("/login-json", func(c *gin.Context) {
 		var json Login
 		//其实就是将request中的Body中的数据按照JSON格式解析到json变量中
 		if err := c.ShouldBindJSON(&json); err != nil {
@@ -26,6 +26,23 @@ func main() {
 			c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
 			return
 		}
+		c.JSON(http.StatusOK, gin.H{"status": "登录成功"})
+	})
+
+	router.POST("/login-form", func(c *gin.Context) {
+		var form Login
+		//方法一：对于FORM数据直接使用Bind函数, 默认使用使用form格式解析,if c.Bind(&form) == nil
+		// 根据请求头中 content-type 自动推断.
+		if err := c.Bind(&form); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if form.Name != "Tony" || form.Password != "123456" {
+			c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+			return
+		}
+
 		c.JSON(http.StatusOK, gin.H{"status": "登录成功"})
 	})
 
