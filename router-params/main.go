0afaf24d04e9bed9ba81a5007dfd5ac887144361
Jsmond2016 <Jsmond2016@gmail.com>
2022-03-26 11:23:54 +0800

    feat: 练习多文件上传

--- router-params/main.go
+++ router-params/main.go
@@ -49,5 +49,27 @@ func main() {
 		c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
 	})
 
+	router.MaxMultipartMemory = 8 << 20 // 8 MiB
+	//router.Static("/", "./public")
+	router.POST("/uploads", func(c *gin.Context) {
+
+		// Multipart form
+		form, err := c.MultipartForm()
+		if err != nil {
+			c.String(http.StatusBadRequest, fmt.Sprintf("get form err: %s", err.Error()))
+			return
+		}
+		files := form.File["files"]
+
+		for _, file := range files {
+			if err := c.SaveUploadedFile(file, file.Filename); err != nil {
+				c.String(http.StatusBadRequest, fmt.Sprintf("upload file err: %s", err.Error()))
+				return
+			}
+		}
+
+		c.String(http.StatusOK, fmt.Sprintf("Uploaded successfully %d files ", len(files)))
+	})
+
 	router.Run()
 }
