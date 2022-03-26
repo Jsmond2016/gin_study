3d73629ce35e2e47511577679474781759087608
Jsmond2016 <Jsmond2016@gmail.com>
2022-03-26 12:00:09 +0800

    feat: chapter-02-uri 绑定

--- chapter-02/main-model.go
+++ chapter-02/main-model.go
@@ -13,7 +13,7 @@ type Login struct {
 
 func main() {
 	router := gin.Default()
-	//1.binding JSON
+	// 1.binding JSON
 	// Example for binding JSON ({"user": "hanru", "password": "hanru123"})
 	router.POST("/login-json", func(c *gin.Context) {
 		var json Login
@@ -29,6 +29,7 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{"status": "登录成功"})
 	})
 
+	// Form 表单绑定
 	router.POST("/login-form", func(c *gin.Context) {
 		var form Login
 		//方法一：对于FORM数据直接使用Bind函数, 默认使用使用form格式解析,if c.Bind(&form) == nil
@@ -44,6 +45,16 @@ func main() {
 		}
 
 		c.JSON(http.StatusOK, gin.H{"status": "登录成功"})
+	})
+
+	// URI 绑定
+	router.GET("/login/:name/:password", func(c *gin.Context) {
+		var login Login
+		if err := c.ShouldBindUri(&login); err != nil {
+			c.JSON(400, gin.H{"msg": err})
+			return
+		}
+		c.JSON(200, gin.H{"name": login.Name, "password": login.Password})
 	})
 
 	router.Run(":8080")
