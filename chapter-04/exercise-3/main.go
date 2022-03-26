d0ba9d602fcf9fc09d0bb958708b8b6c2c60b15e
Jsmond2016 <Jsmond2016@gmail.com>
2022-03-26 16:05:06 +0800

    feat: chapter-4-静态资源服务

---
+++ chapter-04/exercise-3/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	// 下面测试静态文件服务
+
+	// router.StaticFS("/files", http.Dir("../"))
+
+	// Static 提供给定文件系统根目录中的文件。
+	router.Static("/assets", "./assets") // 请求方式：http://localhost:8080/assets/img.png
+	// 显示当前文件夹下的所有文件/或者指定文件
+	router.StaticFS("/show-dir", http.Dir("."))
+	router.StaticFile("/image", "./images/img.png")
+
+	router.Run(":8080")
+}
