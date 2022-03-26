4c243342148d320eb285262398e7f95462c7d5b4
Jsmond2016 <Jsmond2016@gmail.com>
2022-03-26 11:18:22 +0800

    feat: 练习 单个文件上传

--- router-params/main.go
+++ router-params/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -28,6 +29,24 @@ func main() {
 
 		c.String(http.StatusOK, fmt.Sprintf("username is %s, password is %s,hobby is %v", username, password, hobbys))
 
+	})
+
+	router.POST("/upload", func(c *gin.Context) {
+		// single file
+		file, _ := c.FormFile("file")
+		log.Println(file.Filename)
+
+		// Upload the file to specific dst.
+		c.SaveUploadedFile(file, file.Filename)
+
+		/*
+			也可以直接使用io操作，拷贝文件数据。
+			out, err := os.Create(filename)
+			defer out.Close()
+			_, err = io.Copy(out, file)
+		*/
+
+		c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
 	})
 
 	router.Run()
