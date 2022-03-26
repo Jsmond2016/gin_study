21603ace50da8b97416c381cdde56167d74a415f
Jsmond2016 <Jsmond2016@gmail.com>
2022-03-26 10:11:29 +0800

    feat: 自定义HTTP服务器的配置

--- main/main.go
+++ main/main.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello World")
 	})
-	r.Run() // 监听并在 0.0.0.0:8080 上启动服务
+	// r.Run() // 监听并在 0.0.0.0:8080 上启动服务
+	// http.ListenAndServe(":8888", router)
+
+	// 自定义HTTP服务器的配置
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
 }
